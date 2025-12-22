1eafbc453706498ff486f2ec05d8b8a9cde3c311
aman-29-singh <singhaman41004@gmail.com>
2025-12-22 20:06:15 +0530

    grouping of routes in (users) and admin is added

---
+++ nextjs-learning/app/admin/about/page.jsx
@@ -0,0 +1,9 @@
+function AdminAbout(){
+    return(
+        <div>
+            <h1>this is Admin about</h1>
+        </div>
+    )
+}
+
+export default AdminAbout;
---
+++ nextjs-learning/app/admin/page.jsx
@@ -0,0 +1,9 @@
+function Admin(){
+    return (
+        <div>
+            <h1>this is admin page</h1>
+        </div>
+    )
+}
+
+export default Admin;
